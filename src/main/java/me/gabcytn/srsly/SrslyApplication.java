e61e09f8b0f62a60676cd862e369429cc0603f36
gabcytn <cayetanogabriel03@gmail.com>
2026-01-12 23:54:26 +0800

    initial commit

---
+++ src/main/java/me/gabcytn/srsly/SrslyApplication.java
@@ -0,0 +1,12 @@
+package me.gabcytn.srsly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrslyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SrslyApplication.class, args);
+  }
+}
---
+++ src/test/java/me/gabcytn/srsly/SrslyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabcytn.srsly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrslyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
