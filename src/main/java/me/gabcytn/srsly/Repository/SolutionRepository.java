d2bfbb981dbedf192fc39799ff30e2f98d94fcd8
gabcytn <cayetanogabriel03@gmail.com>
2026-01-22 00:52:39 +0800

    feat(srs): initial srs problem on submit of first solution

--- src/main/java/me/gabcytn/srsly/Repository/SolutionRepository.java
+++ src/main/java/me/gabcytn/srsly/Repository/SolutionRepository.java
@@ -1,8 +1,12 @@
 package me.gabcytn.srsly.Repository;
 
+import me.gabcytn.srsly.Entity.Problem;
 import me.gabcytn.srsly.Entity.Solution;
+import me.gabcytn.srsly.Entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SolutionRepository extends CrudRepository<Solution, Integer> {}
+public interface SolutionRepository extends CrudRepository<Solution, Integer> {
+	Boolean existsByProblemAndUser(Problem problem, User user);
+}
