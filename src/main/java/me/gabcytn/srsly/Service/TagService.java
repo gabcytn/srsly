03fc5c210f284e45760e207c873d3b57275572c1
gabcytn <cayetanogabriel03@gmail.com>
2026-02-13 12:44:29 +0800

    refactor: make tag dto a record

--- src/main/java/me/gabcytn/srsly/Service/TagService.java
+++ src/main/java/me/gabcytn/srsly/Service/TagService.java
@@ -15,13 +15,13 @@ public class TagService {
   private final TagRepository tagRepository;
 
   public List<Tag> saveAll(List<TagDto> tags) {
-    List<String> tagNames = tags.stream().map(TagDto::getName).toList();
+    List<String> tagNames = tags.stream().map(TagDto::name).toList();
     List<Tag> existingTags = tagRepository.findByNameIn(tagNames);
     Set<String> existingTagNames =
         existingTags.stream().map(Tag::getName).collect(Collectors.toSet());
     List<Tag> toSave =
         tags.stream()
-            .filter(tag -> !existingTagNames.contains(tag.getName()))
+            .filter(tag -> !existingTagNames.contains(tag.name()))
             .map(TagDto::toEntity)
             .toList();
     if (!toSave.isEmpty()) {
