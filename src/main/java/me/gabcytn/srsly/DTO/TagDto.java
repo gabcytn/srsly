3d67a47c48afb0f8662e472555172a4b45c1eeb7
gabcytn <cayetanogabriel03@gmail.com>
2026-04-04 15:04:07 +0800

    fix: undeleted usage of JsonView annotation

--- src/main/java/me/gabcytn/srsly/DTO/TagDto.java
+++ src/main/java/me/gabcytn/srsly/DTO/TagDto.java
@@ -1,10 +1,8 @@
 package me.gabcytn.srsly.DTO;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import me.gabcytn.srsly.DTO.View.Views;
 import me.gabcytn.srsly.Entity.Tag;
 
-public record TagDto(@JsonView(Views.Summary.class) String name) {
+public record TagDto(String name) {
   public Tag toEntity() {
     return new Tag(name);
   }
